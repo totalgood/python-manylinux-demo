a3f347a299d7ad9bc706bde8c693dd04ad588eac
Hobson Lane <github@totalgood.com>
2018-02-25 19:40:25 -0800

    make test-wheels.sh executable

--- travis/test-wheels.sh
+++ travis/test-wheels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 
-# Install packages and test
+# Install packages (wheels) and test
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install pyaudio3 --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
