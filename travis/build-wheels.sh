0c53bf6011a9d56855e545547c8d97ddeadccc28
Hobson Lane <github@totalgood.com>
2018-02-25 16:28:06 -0800

    dont test in build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -16,8 +16,8 @@ for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install pyaudio3 --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-done
+# # Install packages and test
+# for PYBIN in /opt/python/*/bin/; do
+#     "${PYBIN}/pip" install pyaudio3 --no-index -f /io/wheelhouse
+#     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+# done
